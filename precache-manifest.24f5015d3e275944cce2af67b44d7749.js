117d4973f00a70a41ffeb73a113e6a862af74779
mrclag <mattrclagett@gmail.com>
2020-03-11 16:12:18 -0700

    Updates

---
+++ precache-manifest.24f5015d3e275944cce2af67b44d7749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d64c8d4857939906a4dfddaa3ad887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4825c31d6f0a28804f4",
+    "url": "/static/js/2.bc1a9d68.chunk.js"
+  },
+  {
+    "revision": "67e7c846c02f44cdf485e343802677f5",
+    "url": "/static/js/2.bc1a9d68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c32a0111bdb9ef28cc8",
+    "url": "/static/js/main.3af719a5.chunk.js"
+  },
+  {
+    "revision": "f07d283572a029797789",
+    "url": "/static/js/runtime-main.797212c9.js"
+  },
+  {
+    "revision": "ae797c44cae181c06d11c6fe7332f55c",
+    "url": "/static/media/vocarta_logo_full.ae797c44.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d64042f46190fbb833632cb20347fe89.js"
+  "/precache-manifest.24f5015d3e275944cce2af67b44d7749.js"
 );
 
 self.addEventListener('message', (event) => {
