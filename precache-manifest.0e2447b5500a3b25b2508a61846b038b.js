d3aea3da813feeb16984465755d0b59dbfa7a589
mrclag <mattrclagett@gmail.com>
2020-05-24 23:34:13 -0700

    Updates

---
+++ precache-manifest.0e2447b5500a3b25b2508a61846b038b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a0d403fdd443ed70a682d5fd50b8b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cca516c761284a392cc",
+    "url": "/static/js/2.2fc117e2.chunk.js"
+  },
+  {
+    "revision": "3dc022207f689856d66ecaa56df9eaea",
+    "url": "/static/js/2.2fc117e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d9d9d74b836ff902102",
+    "url": "/static/js/main.2815a5c8.chunk.js"
+  },
+  {
+    "revision": "ba827ae09d757f2f3aba",
+    "url": "/static/js/runtime-main.1a5a8c51.js"
+  },
+  {
+    "revision": "2026cdd34cc3c77d3be6d9d114ddfc23",
+    "url": "/static/media/Group 1.2026cdd3.png"
+  },
+  {
+    "revision": "45b4533f7193d7570bc51b8c66c19245",
+    "url": "/static/media/home-shape.45b4533f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c8266a17127615b7ab88155c46b0c85a.js"
+  "/precache-manifest.0e2447b5500a3b25b2508a61846b038b.js"
 );
 
 self.addEventListener('message', (event) => {
